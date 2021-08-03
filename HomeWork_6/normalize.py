1e44f731243ef1c8095c05f382a1ca226a90ee4a
Dmytro <one.sage888@gmail.com>
2021-08-03 21:39:21 +0300

    Some text

--- HomeWork_6/normalize.py
+++ HomeWork_6/normalize.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import re
 
 CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ'
@@ -16,22 +16,3 @@ def normalize(name: str) -> str:
     trl_name = name.translate(TRANS)
     trl_name = re.sub(r"\W", "_", trl_name)
     return trl_name
-=======
-import re
-
-CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ'
-TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
-               "f", "h", "ts", "ch", "sh", "sch", "", "y", "e", "u", "ja", "je", "i", "ji", "g")
-
-TRANS = {}
-
-for cs, trl in zip(CYRILLIC_SYMBOLS, TRANSLATION):
-    TRANS[ord(cs)] = trl
-    TRANS[ord(cs.upper())] = trl.upper()
-
-
-def normalize(name: str) -> str:
-    trl_name = name.translate(TRANS)
-    trl_name = re.sub(r"\W", "_", trl_name)
-    return trl_name
->>>>>>> 037c4d88185f51b915bb3be8f3da35867d0a1dae
