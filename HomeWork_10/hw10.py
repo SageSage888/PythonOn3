e41c792fa3ffbaa52bd70e13f0e212970338569b
Dmytro <one.sage888@gmail.com>
2021-08-10 18:03:59 +0300

    Home work 10 - start of vygoranie

--- HomeWork_10/hw10.py
+++ HomeWork_10/hw10.py
@@ -1,1 +1,36 @@
-# shotosh
+from collections import UserDict
+
+
+class AddressBook(UserDict):
+    data = {}
+
+    def add_record(self, Record):
+        self.data[Record.name.value] = Record.phone.value
+
+
+class Record:
+    def __init__(self, name, *phones):
+        self.name = name
+        self.phones = phones
+
+    def add():
+        pass
+
+    def delete():
+        pass
+
+    def update():
+        pass
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    pass
